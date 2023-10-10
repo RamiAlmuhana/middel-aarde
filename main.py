65364d48fd58f80b5607be7f29cafd784f4bf44b
David den Hollander <s1148424@student.hsleiden.nl>
2023-10-10 14:18:45 +0200

    made a funny button

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from tkinter import Tk, Label
+from tkinter import *
+
+
+def verander_tekst(label, tekst):
+    label['text'] = tekst
 
 
 def main():
     root = Tk()
-    root.geometry('400x400')
-    hello_world_label = Label(root, text= 'hello world')
+    root.geometry('1400x650')
+    hello_world_label = Label(root, text='hello world')
+    funny_button = Button(root, text='funny', command=lambda: verander_tekst(hello_world_label, type_thing.get()))
+    type_thing = Entry(root, bd=1)
+
     hello_world_label.pack()
+    type_thing.pack()
+    funny_button.pack()
 
     root.mainloop()
 
