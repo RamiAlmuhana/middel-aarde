36d8bfe88e2b96d0edc2303b8a875f17b5c9adca
jasmi_wkrv7zz <s1149346@student.hsleiden.nl>
2023-10-10 15:12:03 +0200

    nieuw

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     def on_new():
         pass
 
+        pass
+
     def on_open():
         pass
 
@@ -27,8 +29,8 @@ def main():
     root.config(menu=menu_bar)
 
     file_menu = Menu(menu_bar, tearoff=0)
-    menu_bar.add_cascade(label="Bestand", menu=file_menu)
-    file_menu.add_command(label="Nieuw", command=on_new)
+    menu_bar.add_cascade(label="Keuze menu", menu=file_menu)
+    file_menu.add_command(label="Niew", command=on_new)
     file_menu.add_command(label="Openen", command=on_open)
     file_menu.add_command(label="Opslaan", command=on_save)
     file_menu.add_separator()
@@ -38,13 +40,23 @@ def main():
     menu_bar.add_cascade(label="Bewerken", menu=edit_menu)
 
     textlabel = Label(root, text='')
-    knop = Button(root, text='button', command=lambda: edit_text(textlabel, 'funny'))
+    knop = Button(root, text='keuzemenu', width=30, command=lambda : edit_text(textlabel, 'funny'))
 
     knop.pack()
     textlabel.pack()
 
+  # dit is een stop knop
+    button = tk.Button(root, text='Stop', width=25, command=root.destroy)
+    button.pack()
+
+
+# dit is een keuzevakje
+    #var1 = IntVar()
+    #Checkbutton(root, text='male', variable=var1).grid(row=0, sticky=W)
+    #var2 = IntVar()
+    #Checkbutton(root, text='female', variable=var2).grid(row=1, sticky=W)
+
+
     root.mainloop()
-
-
 if __name__ == '__main__':
     main()
