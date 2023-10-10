9f3615a9c84f8a97b6ac4ce174bf3e8b64b93d6e
MathieuvanBeek <mathieu.van.beek@gmail.com>
2023-10-10 16:50:38 +0200

    Splashscreen aangemaakt

--- main.py
+++ main.py
@@ -1,62 +1,56 @@
 import tkinter as tk
-from tkinter import *
+import time
 
+def center_window(window):
+    window.update_idletasks()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width - window.winfo_width()) // 2
+    y = (screen_height - window.winfo_height()) // 2
+    window.geometry("+{}+{}".format(x, y))
 
-def edit_text(label, tekst):
-    label['text'] = tekst
+def fade_in():
+    for alpha in range(0, 256, 8):
+        splash_window.attributes("-alpha", alpha / 255)
+        splash_window.update_idletasks()
+        time.sleep(0.02)
 
+def fade_out():
+    for alpha in range(255, -1, -8):
+        splash_window.attributes("-alpha", alpha / 255)
+        splash_window.update_idletasks()
+        time.sleep(0.02)
+
+def change_image():
+    splash_label.configure(image=splash_image2)
+    splash_label.image = splash_image2
+
+    fade_in()
+
+    splash_window.after(5000, fade_out)
 
 def main():
-    def on_new():
-        pass
+    global splash_window
+    splash_window = tk.Tk()
+    splash_window.overrideredirect(True)
+    splash_window.geometry("400x400")
+    center_window(splash_window)
 
-        pass
+    global splash_image1
+    splash_image1 = tk.PhotoImage(file="images/team gandalf logo.png")
 
-    def on_open():
-        pass
+    global splash_image2
+    splash_image2 = tk.PhotoImage(file="images/tolkien estate logo.png")
 
-    def on_save():
-        pass
+    global splash_label
+    splash_label = tk.Label(splash_window, image=splash_image1, borderwidth=0)
+    splash_label.pack()
 
-    def on_exit():
-        root.quit()
+    fade_in()
 
-    root = tk.Tk()
-    root.title("main menu")
-    root.geometry('600x400')
+    splash_window.after(3000, change_image)
 
-    menu_bar = Menu(root)
-    root.config(menu=menu_bar)
+    splash_window.mainloop()
 
-    file_menu = Menu(menu_bar, tearoff=0)
-    menu_bar.add_cascade(label="Keuze menu", menu=file_menu)
-    file_menu.add_command(label="Niew", command=on_new)
-    file_menu.add_command(label="Openen", command=on_open)
-    file_menu.add_command(label="Opslaan", command=on_save)
-    file_menu.add_separator()
-    file_menu.add_command(label="Afsluiten", command=on_exit)
-
-    edit_menu = Menu(menu_bar, tearoff=0)
-    menu_bar.add_cascade(label="Bewerken", menu=edit_menu)
-
-    textlabel = Label(root, text='')
-    knop = Button(root, text='keuzemenu', width=30, command=lambda : edit_text(textlabel, 'funny'))
-
-    knop.pack()
-    textlabel.pack()
-
-  # dit is een stop knop
-    button = tk.Button(root, text='Stop', width=25, command=root.destroy)
-    button.pack()
-
-
-# dit is een keuzevakje
-    #var1 = IntVar()
-    #Checkbutton(root, text='male', variable=var1).grid(row=0, sticky=W)
-    #var2 = IntVar()
-    #Checkbutton(root, text='female', variable=var2).grid(row=1, sticky=W)
-
-
-    root.mainloop()
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
