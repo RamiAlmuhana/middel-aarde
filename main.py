d7af5972cdf8dd8c15a4a66ebeae28f13c3a3ebd
jasmi_wkrv7zz <s1149346@student.hsleiden.nl>
2023-10-10 14:25:05 +0200

    nieuw

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-from tkinter import Tk, Label
+import tkinter as tk
+from tkinter import Menu
 
 
 def main():
-    root = Tk()
-    root.geometry('400x400')
-    hello_world_label = Label(root, text= 'hello world')
-    hello_world_label.pack()
+    def on_new():
+        pass
+    def on_open():
+        pass
+    def on_save():
+        pass
+    def on_exit():
+        root.quit()
+
+
+    root= tk.Tk()
+    root.title("main menu")
+
+    menu_bar = Menu(root)
+    root.config(menu=menu_bar)
+
+    file_menu = Menu(menu_bar, tearoff=0)
+    menu_bar.add_cascade(label="Bestand", menu=file_menu)
+    file_menu.add_command(label="Nieuw", command=on_new)
+    file_menu.add_command(label="Openen", command=on_open)
+    file_menu.add_command(label="Opslaan", command=on_save)
+    file_menu.add_separator()
+    file_menu.add_command(label="Afsluiten", command=on_exit)
+
+    edit_menu = Menu(menu_bar, tearoff=0)
+    menu_bar.add_cascade(label="Bewerken", menu=edit_menu)
+
 
     root.mainloop()
 
