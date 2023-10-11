f376540d82188b8a68597d638e6bb5f5b48e4a0d
David den Hollander <s1148424@student.hsleiden.nl>
2023-10-11 12:12:58 +0200

    deleted my previous work and made a text file template and function to read it

--- verhaalkeuze.py
+++ verhaalkeuze.py
@@ -1,16 +1,40 @@
 from tkinter import Label, Button, Tk
 
 
+def maak_verhaal_dict(verhaal):
+    with open(verhaal) as bestand:
+        verhaaldata = bestand.read()
+    verhaaldata = verhaaldata.splitlines()
+    export = []
+    for line in verhaaldata:
+        split_data = line.split(';;')
+        verhaal_dict = {
+            'naam': split_data[0],
+            'stap': split_data[1],
+            'tekst_1': split_data[2],
+            'plaatje_1': split_data[3],
+            'tekst_2': split_data[4],
+            'plaatje_2': split_data[5],
+            'tekst_3': split_data[6],
+            'plaatje_3': split_data[7],
+            'eig_tekst': split_data[8],
+            'eig_plaatje': split_data[9],
+            'achtergrond': split_data[10],
+            'keuze_1': split_data[11],
+            'keuze_2': split_data[12],
+            'keuze_3': split_data[13],
+        }
+        export.append(verhaal_dict)
+    print(export)
+
+
 def kies_verhaal(root, karakter):
-    from verhaal_1.verhaal_1_1 import verhaal_1
-    from verhaal_2.verhaal_2_1 import verhaal_2
-    from verhaal_3.verhaal_3_1 import verhaal_3
     for widget in root.winfo_children():
         widget.destroy()
     instructie = Label(root, text='In welk verhaal wil je je verdiepen?')
-    button_1 = Button(root, text='verhaal 1', command=lambda: verhaal_1(root, karakter))
-    button_2 = Button(root, text='verhaal 2', command=lambda: verhaal_2(root, karakter))
-    button_3 = Button(root, text='verhaal 3', command=lambda: verhaal_3(root, karakter))
+    button_1 = Button(root, text='verhaal 1', command=lambda: maak_verhaal_dict('verhaal_1.txt'))
+    button_2 = Button(root, text='verhaal 2', command=lambda: maak_verhaal_dict('verhaal_2.txt'))
+    button_3 = Button(root, text='verhaal 3', command=lambda: maak_verhaal_dict('verhaal_3.txt'))
 
     instructie.pack()
     button_1.pack()
@@ -22,7 +46,7 @@ def kies_verhaal(root, karakter):
 
 def test_start():
     root = Tk()
-    root.geometry('400x400')
+    root.geometry('1400x700')
     kies_verhaal(root, 'frodo')
 
 
