38318108ea44a7ddc2ee7cf584991838b8788af9
Sem Bersee <s1148541@student.hsleiden.nl>
2023-10-11 12:57:44 +0200

    music starts when starting and you can turn it on and off on the button

--- audio.py
+++ audio.py
@@ -12,20 +12,25 @@ def button_play():
 def music_play():
     pygame.mixer.init()
     pygame.mixer.music.load("audio/1.mp3")
-    pygame.mixer.music.play()
+    pygame.mixer.music.play(loops=-1)
     pygame.mixer.music.set_volume(0.5)
+
+
+def music_stop():
+    pygame.mixer.music.stop()
 
 
 def test_menu():
     root = tk.Tk()
     root.title("Muziek")
-    button = tk.Button(root, text="Klik voor button geluidje", command=button_play)
+    button = tk.Button(root, text="Klik voor button geluidje", command=music_play)
     root.geometry('400x400')
     button.pack()
     root.mainloop()
 
 
 def main():
+    music_play()
     test_menu()
 
 
