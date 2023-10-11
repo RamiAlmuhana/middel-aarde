eec820645c89a0a57f96b59a4d539bac8e2dadf3
jasmi_wkrv7zz <s1149346@student.hsleiden.nl>
2023-10-11 14:33:17 +0200

    laatste wijzigingen

--- karakter_keuze.py
+++ karakter_keuze.py
@@ -2,17 +2,37 @@ from tkinter import Tk, Label, Button, Frame
 from scherm_1 import *
 
 def maak_karakter_dict():
+    with open("tekstbestand.txt") as bestand:
+        verhaaldata = bestand.read()
+    verhaaldata = verhaaldata.splitlines()
+    export = []
+    for line in verhaaldata:
+        split_data = line.split(';;')
+        verhaal_dict = {
+            "naam": split_data[0],
+            "ras": split_data[1],
+            "beschrijving": split_data[2],
+            "specialiteit": split_data[3],
+            "foto": split_data[4]
+        }
+        export.append(verhaal_dict)
+    return export
+
 
 def kies_karakter(venster):
+
+    karakter_data = maak_karakter_dict()
     for widget in venster.winfo_children():
         widget.destroy()
 
     knoppen_frame = Frame(venster)
     knoppen_frame.pack(expand=True)
 
-    button_1 = Button(knoppen_frame, text="Aragorn", width=30, height=7, command=lambda: kies_karakter_1(venster))
-    button_2 = Button(knoppen_frame, text="Gandalf", width=30, height=7, command=lambda: kies_karakter_2(venster))
-    button_3 = Button(knoppen_frame, text="Frodo", width=30, height=7, command=lambda: kies_karakter_3(venster))
+    button_font = ("Helvetica", 12, "bold")
+
+    button_1 = Button(knoppen_frame, text=karakter_data[0]['naam'], width=30, height=7, command=lambda: karakter_gekozen(venster, karakter_data[0]), font=button_font)
+    button_2 = Button(knoppen_frame, text=karakter_data[1]['naam'], width=30, height=7, command=lambda: karakter_gekozen(venster, karakter_data[1]), font=button_font)
+    button_3 = Button(knoppen_frame, text=karakter_data[2]['naam'], width=30, height=7, command=lambda: karakter_gekozen(venster, karakter_data[2]), font=button_font)
 
     button_1.pack(side="left", padx=10)
     button_2.pack(side="left", padx=10)
--- scherm_1.py
+++ scherm_1.py
@@ -1,33 +1,19 @@
 from tkinter import Button, Label
 
 
-def kies_karakter_1(venster):
+def karakter_gekozen(venster, karakter):
     for widget in venster.winfo_children():
         widget.destroy()
-    label_1 = Label(venster, text="Wilt u verder?", width=30, height=7)
-    button1 = Button(venster, text= "Ja", width=30, height=7)
-    button2 = Button(venster, text= "Nee",width=30, height=7, command= lambda: on_back_button_clicked(venster))
+    label_font = ("Helvetica", 14, "bold")
+    karakternaam = Label(venster, text=karakter['naam'] + '\n' + karakter["beschrijving"] + '\n' +karakter["specialiteit"] + '\n' + karakter["ras"] + '\n', font=label_font)
+    label_1 = Label(venster, text="Wilt u verder?", width=30, height=7, font=("Helvetica", 12))
+    button1 = Button(venster, text="Ja", width=30, height=7, font=("Helvetica", 12))
+    button2 = Button(venster, text="Nee", width=30, height=7, command=lambda: on_back_button_clicked(venster), font=("Helvetica", 12))
 
-
-    label_1.pack(side="top", pady= 50, padx=10)
-    button1.pack(side="left", pady= 50, padx=10)
-    button2.pack(side="left",pady= 50, padx=10)
-
-
-def kies_karakter_2(venster):
-    for wiget in venster.winfo_children():
-        wiget.destroy()
-    label_1 = Label(venster, text="Wilt u verder?")
-
-    label_1.pack(side="left", padx=10)
-
-
-def kies_karakter_3(venster):
-    for widget in venster.winfo_children():
-        widget.destroy()
-    label_1 = Label(venster, text="Wilt u verder?")
-
-    label_1.pack(side="left", padx=10)
+    karakternaam.pack()
+    label_1.pack(side="top", pady=50, padx=10)
+    button1.pack(side="left", pady=50, padx=10)
+    button2.pack(side="left", pady=50, padx=10)
 
 
 def on_back_button_clicked(venster):
