f19283fbf0aa74feee447f71de9798d9f2c2c3a6
Sem Bersee <s1148541@student.hsleiden.nl>
2023-10-11 09:38:31 +0200

    added button sound when you click a button

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import tkinter as tk
 import pygame
 
-pygame.mixer.init()
-pygame.mixer.music.load("audio/1.mp3")
-pygame.mixer.music.play()
-pygame.mixer.music.set_volume(0.5)
+
+def button_play():
+    pygame.mixer.init()
+    pygame.mixer.music.load("audio/button_sound.mp3")
+    pygame.mixer.music.play()
+    pygame.mixer.music.set_volume(1)
 
 
-root = tk.Tk()
-root.title("Muziek")
+def music_play():
+    pygame.mixer.init()
+    pygame.mixer.music.load("audio/1.mp3")
+    pygame.mixer.music.play()
+    pygame.mixer.music.set_volume(0.5)
 
-play_button = tk.Button(root, text="Hoi")
-play_button.pack()
 
-root.mainloop()
+def test_menu():
+    root = tk.Tk()
+    root.title("Muziek")
+    button = tk.Button(root, text="Klik voor button geluidje", command=button_play)
+    root.geometry('400x400')
+    button.pack()
+    root.mainloop()
+
+
+def main():
+    test_menu()
+
+
+if __name__ == '__main__':
+    main()
