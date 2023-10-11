65134e5d6772f49ba5b2b85033a157542e80f52d
MathieuvanBeek <mathieu.van.beek@gmail.com>
2023-10-11 12:55:54 +0200

    Main menu gemaakt

--- main.py
+++ main.py
@@ -1,6 +1,51 @@
-def main():
-    print("Hi")
+import tkinter as tk
+import tkinter.font as tkFont
 
 
-if __name__ == '__main__':
-    main()
+def knop_sluiten(main_window):
+    main_window.destroy()
+
+
+def center_window(window):
+    window.update_idletasks()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width - window.winfo_width()) // 2
+    y = (screen_height - window.winfo_height()) // 2
+    window.geometry("+{}+{}".format(x, y))
+
+
+def main_app():
+    main_window = tk.Tk()
+    main_window.title("Lord of the Rings")
+    main_window.geometry("1400x700")
+    center_window(main_window)
+    main_window.configure(bg="darkgreen")
+
+    font = ("Helvetica", 16)
+
+    spel_naam = tk.Label(text='Lord of the Rings', bg='darkgreen', font=("Helvetica", 32))
+    spel_naam.place(x=525, y=75)
+
+    personage_kiezen = tk.Button(main_window, text="Personage kiezen", bg='lightgreen', width=55, height=2)
+    personage_kiezen.place(x=375, y=175)
+    personage_kiezen.config(font=font)
+
+    personage_aanmaken = tk.Button(main_window, text="Personage aanmaken", bg='lightgreen', width=55, height=2)
+    personage_aanmaken.place(x=375, y=275)
+    personage_aanmaken.config(font=font)
+
+    instellingen = tk.Button(main_window, text="Instellingen", bg='lightgreen', width=55, height=2)
+    instellingen.place(x=375, y=375)
+    instellingen.config(font=font)
+
+    sluiten = tk.Button(main_window, text="Sluiten", command=lambda: knop_sluiten(main_window), bg='lightgreen',
+                        width=55, height=2)
+    sluiten.place(x=375, y=475)
+    sluiten.config(font=font)
+
+    main_window.mainloop()
+
+
+if __name__ == "__main__":
+    main_app()
