eff2a00c437dfc4e7173c8f026a6f8d442956c41
Rami Al-Muhana <s1148232@student.hsleiden.nl>
2023-10-12 18:34:20 +0200

    Fixed an error that was in the karakter keuze menu

--- karakter_keuze.py
+++ karakter_keuze.py
@@ -26,33 +26,26 @@ def kies_karakter(venster):
     for widget in venster.winfo_children():
         widget.destroy()
 
-    # venster = Frame(venster)
-    # venster.pack(expand=True)
-
-    button_font = ("Footlight MT Light", 12, "bold")
-    label1 = Label(knoppen_frame, text="Welk karakter wilt u kiezen?", font=("Footlight MT Light", 18))
-    button_1 = Button(knoppen_frame, text=karakter_data[0]['naam'], width=30, height=7,
-                      command=lambda: karakter_gekozen(venster, karakter_data[0]), font=button_font)
-    button_2 = Button(knoppen_frame, text=karakter_data[1]['naam'], width=30, height=7,
-                      command=lambda: karakter_gekozen(venster, karakter_data[1]), font=button_font)
-    button_3 = Button(knoppen_frame, text=karakter_data[2]['naam'], width=30, height=7,
-                      command=lambda: karakter_gekozen(venster, karakter_data[2]), font=button_font)
+    label1 = Label(venster, text="Welk karakter wilt u kiezen?", font=("Footlight MT Light", 18))
+    button_1_image = PhotoImage(file="images/frodo.png")
+    button_1 = Button(venster, image=button_1_image, command=lambda: karakter_gekozen(venster, karakter_data[1]))
+    button_1_image.image = button_1_image
+    button_1.bind("<Button-1>", lambda event: karakter_gekozen(venster, karakter_data[0]))
+    button_1.pack()
 
     button_2_image = PhotoImage(file="images/gimli.png")
-    button_2 = Button(knoppen_frame, image=button_2_image, command=lambda: karakter_gekozen(venster, karakter_data[1]))
+    button_2 = Button(venster, image=button_2_image, command=lambda: karakter_gekozen(venster, karakter_data[1]))
     button_2_image.image = button_2_image
     button_2.bind("<Button-1>", lambda event: karakter_gekozen(venster, karakter_data[1]))
     button_2.pack()
 
     button_3_image = PhotoImage(file="images/legolas.png")
-    button_3 = Button(knoppen_frame, image=button_3_image, command=lambda: karakter_gekozen(venster, karakter_data[2]))
+    button_3 = Button(venster, image=button_3_image, command=lambda: karakter_gekozen(venster, karakter_data[2]))
     button_3_image.image = button_3_image
     button_3.bind("<Button-1>", lambda event: karakter_gekozen(venster, karakter_data[2]))
     button_3.pack()
 
-    label1.place()
-    karakternaam.place(x=100, y=500)
+    label1.place(x=550, y=50)
     button_1.pack(side="left", padx=10)
     button_2.pack(side="left", padx=100)
     button_3.pack(side="left", padx=10)
-    button_hoofdmenu.place(x=1250, y=600)
