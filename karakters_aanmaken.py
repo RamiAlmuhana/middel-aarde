6f63b44d4721e1057362eea2ab80b0a157b3faec
Rami Al-Muhana <s1148232@student.hsleiden.nl>
2023-10-12 14:14:25 +0200

    Created karakter function made startfile so the app start from there

--- karakters_aanmaken.py
+++ karakters_aanmaken.py
@@ -1,6 +1,7 @@
 from tkinter import Tk, Label, Entry, OptionMenu, StringVar, Button
 import tkinter as tk
 
+
 def personages_wegschrijven(text_File, selected_rassen, selected_eigenschappen):
     with open("karakters.txt", "a") as file:
         user_Input = text_File.get()
@@ -9,38 +10,24 @@ def personages_wegschrijven(text_File, selected_rassen, selected_eigenschappen):
         file.write(f"{user_Input};{selected_option_rassen};{selected_option_eigenschappen}\n")
         text_File.delete(0, tk.END)
 
-
 def karakter_aanmaken(main_window):
+    from main import create_main_app_frame
     karakter_aanmaken_frame = tk.Frame(main_window)
     karakter_aanmaken_frame.place(x=0, y=0, relwidth=1, relheight=1)
     main_window.title("Personage aangemaakt")
 
     # Bevestigingsscherm maken
-    confirmation_label = Label(main_window, text="Personage is aangemaakt!", font=('Helvetica bold', 20))
-    confirmation_label.pack(pady=50)
+    # confirmation_label = Label(main_window, text="Personage is aangemaakt!", font=('Helvetica bold', 20))
+    # confirmation_label.pack(pady=50)
 
     # Terugknop naar het beginscherm
     back_button = Button(main_window, text="Naar het hoofdmenu", command=lambda: main_window())
     back_button.pack()
-    # Buttons
-    # terug_knop = Button(main_window, text="Terug", command=lambda: hoofdmenu_scherm())
-    # aanmaak_knop = Button(main_window, text="Aanmaken", command=lambda: personages_wegschrijven(gebruikers_input, rassen_clicked, eigenschappen_clicked))
-
-
-def karakters_scherm_aanmaken(main_window):
-    karakters_scherm_aanmaken = tk.Frame(main_window)
-    karakters_scherm_aanmaken.place(x=0, y=0, relwidth=1, relheight=1)
-    # Labels
-    titel = Label(main_window, text="Karakter aanmaken", font=('Helvetica bold', 20))
-    karakter_naam_kiezen = Label(main_window, text="Naam kiezen")
-    Karakter_ras_kiezen = Label(main_window, text="Rassen")
-    Karakter_eigenschap_kiezen = Label(main_window, text="Eigenschappen")
 
     gebruikers_input = Entry(main_window)
 
-
     rassen = ["Mens", "Elf", "Hobbit", "Dwerg"]
-    eigenschappen = ["Supersterk", "Zwaard", "Boog", "Magie", "Toverstaf"]
+    eigenschappen = ["Bijl", "Boog", "Superslim"]
 
     rassen_clicked = StringVar()
     eigenschappen_clicked = StringVar()
@@ -51,6 +38,23 @@ def karakters_scherm_aanmaken(main_window):
     rassen_menu = OptionMenu(main_window, rassen_clicked, *rassen)
     eigenschappen_menu = OptionMenu(main_window, eigenschappen_clicked, *eigenschappen)
 
+    # Buttons
+    font = ("Helvetica", 16)
+    terugknop = tk.Button(main_window, text="Terug naar main menu", bg='lightgreen', width=55, height=2,
+                          command=lambda: create_main_app_frame(main_window), font=font)
+    terugknop.place(x=400, y=400)
+    aanmaak_knop = Button(main_window, text="Aanmaken",
+                          command=lambda: personages_wegschrijven(gebruikers_input, rassen_clicked,
+                                                                  eigenschappen_clicked))
+
+    # karakters_scherm_aanmaken = tk.Frame(main_window)
+    # karakters_scherm_aanmaken.place(x=0, y=0, relwidth=1, relheight=1)
+    # Labels
+    titel = Label(main_window, text="Karakter aanmaken", font=('Helvetica bold', 20))
+    karakter_naam_kiezen = Label(main_window, text="Naam kiezen")
+    Karakter_ras_kiezen = Label(main_window, text="Rassen")
+    Karakter_eigenschap_kiezen = Label(main_window, text="Eigenschappen")
+
     karakter_naam_kiezen.place(relx=0.5, rely=0.25, anchor="center")
     gebruikers_input.place(relx=0.5, rely=0.3, anchor="center")
     Karakter_ras_kiezen.place(relx=0.5, rely=0.38, anchor="center")
@@ -58,16 +62,5 @@ def karakters_scherm_aanmaken(main_window):
     Karakter_eigenschap_kiezen.place(relx=0.5, rely=0.55, anchor="center")
     eigenschappen_menu.place(relx=0.5, rely=0.62, anchor="center")
     titel.pack()
-    terug_knop.place(relx=0.25, rely=0.75, anchor="center")
-    # aanmaak_knop.place(relx=0.75, rely=0.75, anchor="center")
-
-
-
-#
-# def main(main_window):
-
-
-
-if __name__ == '__main__':
-    karakters_scherm_aanmaken()
-    main_window.mainloop()
+    # terug_knop.place(relx=0.25, rely=0.75, anchor="center")
+    aanmaak_knop.place(relx=0.75, rely=0.75, anchor="center")
---
+++ startfile.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from main import center_window
+import splashscreen
+
+
+def start_window():
+    main_window = tk.Tk()
+    main_window.title("Lord of the Rings")
+    main_window.geometry("1400x700")
+    center_window(main_window)
+    main_window.resizable(False, False)
+
+    main_window.mainloop()
+
+
+if __name__ == "__main__":
+    splashscreen.main()
+    start_window()
