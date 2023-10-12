4d6f7bd18aa252fc8a73c3b105c973f4f6b47d74
David den Hollander <s1148424@student.hsleiden.nl>
2023-10-12 09:48:38 +0200

    some formatting

--- keuzemomenten.py
+++ keuzemomenten.py
@@ -13,16 +13,16 @@ def maak_keuzemoment(root, karakter, verhaal, stage, keuzetekst, keuzeplaatje, e
         else:
             verhaaltekst = Label(root, text=verhaal[stage][keuzetekst], font=("Footlight MT Light", 16))
         #     verhaalplaatje = verhaal[stage][keuzeplaatje]
-        button_1 = Button(root, text=verhaal[stage]['keuze_1'], width=30, height=7, command=lambda: maak_keuzemoment(root, karakter, verhaal, verhaal[stage]['stap'], 'tekst_1', 'plaatje_1', ''), font=button_font)
-        button_2 = Button(root, text=verhaal[stage]['keuze_2'], width=30, height=7, command=lambda: maak_keuzemoment(root, karakter, verhaal, verhaal[stage]['stap'], 'tekst_2', 'plaatje_2', ''), font=button_font)
-        button_3 = Button(root, text=verhaal[stage]['keuze_3'], width=30, height=7, command=lambda: maak_keuzemoment(root, karakter, verhaal, verhaal[stage]['stap'], 'tekst_3', 'plaatje_3', verhaal[stage]['eigenschap']), font=button_font)
+        button_1 = Button(root, text=verhaal[stage]['keuze_1'], width=40, height=7, command=lambda: maak_keuzemoment(root, karakter, verhaal, verhaal[stage]['stap'], 'tekst_1', 'plaatje_1', ''), font=button_font)
+        button_2 = Button(root, text=verhaal[stage]['keuze_2'], width=40, height=7, command=lambda: maak_keuzemoment(root, karakter, verhaal, verhaal[stage]['stap'], 'tekst_2', 'plaatje_2', ''), font=button_font)
+        button_3 = Button(root, text=verhaal[stage]['keuze_3'], width=40, height=7, command=lambda: maak_keuzemoment(root, karakter, verhaal, verhaal[stage]['stap'], 'tekst_3', 'plaatje_3', verhaal[stage]['eigenschap']), font=button_font)
 
         progress.pack()
         verhaaltekst.pack()
         # verhaalplaatje.pack()
         if '   ' not in verhaaltekst['text']:
             button_1.pack(side="left", pady=50, padx=10)
-            button_2.pack(side="left", pady=50, padx=250)
+            button_2.pack(side="left", pady=50, padx=20)
             button_3.pack(side="left", pady=50, padx=10)
         else:
             dood = Label(root, text='je hebt verloren')
